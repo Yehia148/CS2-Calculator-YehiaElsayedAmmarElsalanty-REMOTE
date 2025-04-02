7377721ce14550c6aa86290f74a83339e0fd18d6
Yehia148 <yahialsayed@aucegypt.edu>
2025-04-03 01:58:24 +0200

    commit for the header file of the calculator

---
+++ calculator.h
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//These are the functions of the calculator in the header file.
+double addition(double x, double y);
+double subtract(double x, double y);
+double multiply(double x, double y);
+double divide(double x, double y);
+int factorial(int x);
+int GCD(int x, int y);
+void LCM(int x, int y);
+int RNG(int x, int y);
